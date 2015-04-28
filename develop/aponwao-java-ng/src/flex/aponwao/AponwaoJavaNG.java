70f6b144e3723cbb9dde03043efae644d55898fe
Felix Lopez <flex.developments@gmail.com>
2015-04-28 04:53:06 -0300

    Reconfiguracion para ambiente de desarrollo

---
+++ develop/aponwao-java-ng/src/flex/aponwao/AponwaoJavaNG.java
@@ -0,0 +1,31 @@
+/*
+ * aponwao-java-ng
+ * 
+ * Copyright (C) 2015
+ * Ing. Felix D. Lopez M. - flex.developments en gmail.com
+ *
+ * Este programa es software libre; Usted puede usarlo bajo los terminos de la
+ * licencia de software GPL version 2.0 de la Free Software Foundation.
+ *
+ * Este programa se distribuye con la esperanza de que sea util, pero SIN
+ * NINGUNA GARANTIA; tampoco las implicitas garantias de MERCANTILIDAD o
+ * ADECUACION A UN PROPOSITO PARTICULAR.
+ * Consulte la licencia GPL para mas detalles. Usted debe recibir una copia
+ * de la GPL junto con este programa; si no, escriba a la Free Software
+ * Foundation Inc. 51 Franklin Street,5 Piso, Boston, MA 02110-1301, USA.
+ */
+
+package flex.aponwao;
+
+/**
+ * AponwaoJavaNG
+ * 
+ * @author Ing. Felix D. Lopez M. - flex.developments en gmail.com
+ */
+public class AponwaoJavaNG {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
