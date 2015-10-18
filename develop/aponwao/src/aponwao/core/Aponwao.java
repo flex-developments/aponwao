136eea04ed0891d2d6f1779a160589a1a0461ebf
Felix Lopez <flex.developments@gmail.com>
2015-10-18 17:22:12 -0300

    New project

---
+++ develop/aponwao/src/aponwao/core/Aponwao.java
@@ -0,0 +1,32 @@
+/*
+ * aponwao
+ * 
+ * Copyright (C) 2015
+ * Ing. Felix D. Lopez M. - flex.developments en gmail.com
+ * 
+ * Este programa es software libre; Usted puede usarlo bajo los terminos de la
+ * licencia de software GPL version 2.0 de la Free Software Foundation.
+ * 
+ * Este programa se distribuye con la esperanza de que sea util, pero SIN
+ * NINGUNA GARANTIA; tampoco las implicitas garantias de MERCANTILIDAD o
+ * ADECUACION A UN PROPOSITO PARTICULAR.
+ * Consulte la licencia GPL para mas detalles. Usted debe recibir una copia
+ * de la GPL junto con este programa; si no, escriba a la Free Software
+ * Foundation Inc. 51 Franklin Street,5 Piso, Boston, MA 02110-1301, USA.
+ */
+package aponwao.core;
+
+/**
+ *
+ * @author Ing. Felix D. Lopez M. - flex.developments en gmail.com
+ */
+public class Aponwao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
